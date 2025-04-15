e8a70578c50be4ede48d9f9682f3e81fb9249876
feltroidprime <96737978+feltroidprime@users.noreply.github.com>
2025-04-15 11:37:16 +0200

    Implement SWUConfig for curve configuration with ZETA constant

--- curves/starknet-curve/src/curves/mod.rs
+++ curves/starknet-curve/src/curves/mod.rs
@@ -1,10 +1,10 @@
+use crate::{fq::Fq, fr::Fr};
+use ark_ec::hashing::curve_maps::swu::SWUConfig;
 use ark_ec::{
     models::CurveConfig,
     short_weierstrass::{self as sw, SWCurveConfig},
 };
-use ark_ff::{AdditiveGroup, Field, MontFp, Zero};
-
-use crate::{fq::Fq, fr::Fr};
+use ark_ff::{Field, MontFp, Zero};
 
 #[cfg(test)]
 mod tests;
@@ -25,6 +25,33 @@ impl CurveConfig for Config {
     /// COFACTOR_INV = COFACTOR^{-1} mod r = 1
     #[rustfmt::skip]
     const COFACTOR_INV: Fr =  Fr::ONE;
+}
+
+impl SWUConfig for Config {
+    // def find_z_sswu(F, A, B):
+    //     R.<xx> = F[]                       # Polynomial ring over F
+    //     g = xx^3 + F(A) * xx + F(B)        # y^2 = g(x) = x^3 + A * x + B
+    //     ctr = F.gen()
+    //     while True:
+    //         for Z_cand in (F(ctr), F(-ctr)):
+    //             # Criterion 1: Z is non-square in F.
+    //             if is_square(Z_cand):
+    //                 continue
+    //             # Criterion 2: Z != -1 in F.
+    //             if Z_cand == F(-1):
+    //                 continue
+    //             # Criterion 3: g(x) - Z is irreducible over F.
+    //             if not (g - Z_cand).is_irreducible():
+    //                 continue
+    //             # Criterion 4: g(B / (Z * A)) is square in F.
+    //             if is_square(g(B / (Z_cand * A))):
+    //                 return Z_cand
+    //         ctr += 1
+    // F = GF(3618502788666131213697322783095070105623107215331596699973092056135872020481)
+    // a = 1
+    // b = 3141592653589793238462643383279502884197169399375105820974944592307816406665
+    // find_z_sswu(F, a, b) == 19
+    const ZETA: Fq = MontFp!("19");
 }
 
 impl SWCurveConfig for Config {
